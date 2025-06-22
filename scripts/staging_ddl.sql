5aee08b87d7d6efe0990ffb57282faf4c3e57f58
Kunal Mahendra Pawar <90176089+Pawarkunal@users.noreply.github.com>
2025-06-22 10:26:43 +0530

    Update staging_ddl.sql

--- scripts/staging_ddl.sql
+++ scripts/staging_ddl.sql
@@ -11,20 +11,20 @@ Script Purpose:
 
 DROP TABLE IF EXISTS staging.crm_cust_info;
 CREATE TABLE staging.crm_cust_info (
-	cst_id 				INT,
-	cst_key 			VARCHAR(50),
+	cst_id 			INT,
+	cst_key 		VARCHAR(50),
 	cst_firstname 		VARCHAR(50),
 	cst_lastname 		VARCHAR(50),
 	cst_marital_status 	VARCHAR(50),
-	cst_gndr 			VARCHAR(50),
+	cst_gndr 		VARCHAR(50),
 	cst_create_date 	DATE
 );
 
 DROP TABLE IF EXISTS staging.crm_prd_info;
 CREATE TABLE staging.crm_prd_info (
-	prd_id 			INT,
+    prd_id 		INT,
     prd_key 		VARCHAR(50),
-    prd_nm 			VARCHAR(50),
+    prd_nm 		VARCHAR(50),
     prd_cost 		INT,
     prd_line 		VARCHAR(50),
     prd_start_dt 	DATETIME,
@@ -33,7 +33,7 @@ CREATE TABLE staging.crm_prd_info (
 
 DROP TABLE IF EXISTS staging.crm_sales_details;
 CREATE TABLE staging.crm_sales_details (
-	sls_ord_num  VARCHAR(50),
+    sls_ord_num  VARCHAR(50),
     sls_prd_key  VARCHAR(50),
     sls_cust_id  INT,
     sls_order_dt INT,
