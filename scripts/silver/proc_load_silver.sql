0818c4b341c0a897a593d9901bcd798db2aef91f
Kunal Mahendra Pawar <90176089+Pawarkunal@users.noreply.github.com>
2025-06-23 20:47:30 +0530

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -144,3 +144,18 @@ SELECT
 		ELSE TRIM(REPLACE(REPLACE(REPLACE(cntry, CHAR(9), ''), CHAR(10), ''), CHAR(13), ''))
 	END AS cntry -- Normalize and handle null, missing countries
 FROM bronze.erp_loc_a101;
+
+-----------------------------------------
+
+TRUNCATE TABLE silver.erp_px_cat_g1v2;
+INSERT INTO silver.erp_px_cat_g1v2(
+	id,
+    cat,
+    subcat,
+    maintenance)
+SELECT
+	id,
+    cat,
+    subcat,
+    maintenance
+FROM bronze.erp_px_cat_g1v2;
